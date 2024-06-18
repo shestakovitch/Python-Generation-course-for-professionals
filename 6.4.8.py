4bf608918e687d7257a544d214356b677f37d76c
shestakovitch <shestakovitch@gmail.com>
2024-06-18 14:52:09 +0200

    именованный кортеж Fruit с полями name, color и vitamins

---
+++ 6.4.8.py
@@ -0,0 +1,7 @@
+"""Дополните приведенный ниже код, чтобы он создал именованный кортеж Fruit с полями name, color и vitamins.
+
+Примечание. Программа ничего не должна выводить."""
+
+from collections import namedtuple
+
+Fruit = namedtuple('Fruit', ('name', 'color', 'vitamins'))
