f0dac7a11bc0ae9bd9379c06ec10002f963f4e74
shestakovitch <shestakovitch@gmail.com>
2024-06-18 17:37:07 +0200

    элементы следующему правилу

---
+++ 6.6.18.py
@@ -0,0 +1,23 @@
+"""Вам доступен словарь data с четным количеством элементов. Дополните приведенный ниже код, чтобы он вывел данный
+словарь, расположив его элементы по следующему правилу: первый, последний, второй, предпоследний, третий, и так далее.
+
+Примечание. Например, если бы словарь data имел вид:
+
+data = OrderedDict(key1='value1', key2='value2', key3='value3', key4='value4')
+то программа должна была бы вывести (до Python 3.12):
+
+OrderedDict([('key1', 'value1'), ('key4', 'value4'), ('key2', 'value2'), ('key3', 'value3')])
+в Python 3.12:
+
+OrderedDict({('key1', 'value1'), ('key4', 'value4'), ('key2', 'value2'), ('key3', 'value3')})"""
+
+from collections import OrderedDict
+
+data = OrderedDict({'Name': 'Брусника', 'IsNetObject': 'да', 'OperatingCompany': 'Брусника', 'TypeObject': 'кафе',
+                    'AdmArea': 'Центральный административный округ', 'District': 'район Арбат',
+                    'Address': 'город Москва, переулок Сивцев Вражек, дом 6/2', 'SeatsCount': '10'})
+new_data = OrderedDict()
+for rule in (False, True) * int(len(data) / 2):
+    key, value = data.popitem(last=rule)
+    new_data[key] = value
+print(new_data)
